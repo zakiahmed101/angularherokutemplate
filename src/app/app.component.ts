5d6ec998a009eb875d18ec9b40b4aac06dc64ea9
zakiahmed101 <zaki_ahmed101@hotmail.co.uk>
2017-10-26 01:18:49 +0100

    deploy to github

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Shakeel Traders Haven is gonna be boom';
+  title = 'zaki';
 }
